6eba9593d035cfc219cf7391e15debcc6773796f
Juan Manoel <120132154+JuManoel@users.noreply.github.com>
2024-01-27 17:09:40 -0300

    Change in the app.js code.
    I fix that bug in generating number

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let numeroSecreto;//o numero secreto
 let cont;//contador de intentos
 let gramatica;
 let listaNumSorteados=[];
-
+let contBucle=0;
 comecarJogo();
 
 function comecarJogo(){
@@ -11,22 +11,25 @@ function comecarJogo(){
     mudarHTML("h1","Juego del Número Secreto")
     mudarHTML("p","Elija un numero de 1 a 10")
     numeroSecreto=generarNumeroSecreto();//o numero secreto
+
     cont=0;//contador de intentos
     gramatica="intento";
+    if(listaNumSorteados.length==10){
+        listaNumSorteados=[];
+    }
 }
 
 function generarNumeroSecreto() {
     //gerador do numero
     let numeroGerado=Math.floor(Math.random()*10)+1;
-    if(listaNumSorteados.includes(numeroGerado)){
-        generarNumeroSecreto();
-    }else{
-        listaNumSorteados.push(numeroGerado);
-        if(listaNumSorteados.length==10){
-            listaNumSorteados=[];
-        }
-        return numeroGerado;
+    while(listaNumSorteados.includes(numeroGerado)){
+        numeroGerado=Math.floor(Math.random()*10)+1;
+        contBucle++;
     }
+    listaNumSorteados.push(numeroGerado);
+    contBucle=0;
+    return numeroGerado;
+    
 }
 
 function verificarIntento(){
